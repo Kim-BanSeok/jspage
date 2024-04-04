8ed4ca6b2d213b762e1b1a8ecacf8c908ae324f5
bbb <qkstjr0000@naver.com>
2024-04-04 12:18:43 +0900

    update index.html and script.js

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-//document.getElementsByClassName('d').style.color="yellow";
-var aa = document.getElementsByClassName("d")[0];
-aa.style.color = "yellow";
+document.getElementsByClassName('d').style.color="yellow";
+
