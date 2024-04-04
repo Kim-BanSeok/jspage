8fc9b616922411597b09410422d49e05abcaddbc
bbb <qkstjr0000@naver.com>
2024-04-04 17:41:34 +0900

    feat: create javascript01.js

---
+++ javascript01.js
@@ -0,0 +1,26 @@
+// 자바스크립트 변수, 자료형, 조건문, 반복문
+
+//변수
+let craam = 'a';
+const choco = 'b';
+
+let camelCase; 카멜케이스
+let snake_case; 스네이크케이스
+let PascalCase; 파스칼케이스
+//let $case1; $ 달러
+//let _case2; _ 밑줄
+
+let bread;
+let creamb = 'cream';
+bread = creamb;
+console.log(bread);
+
+//자료형
+
+
+
+//함수
+
+//조건문
+
+//반복문
