1c920f18c169264b46a96916b2eb58b86d541568
bbb <qkstjr0000@naver.com>
2024-04-04 11:46:05 +0900

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //document.getElementsByClassName('d').style.color="yellow";
-var aa = document.getElementsByClassName('d');
+var aa = document.getElementsByClassName('d')[0];
 aa.style.color = "yellow";
